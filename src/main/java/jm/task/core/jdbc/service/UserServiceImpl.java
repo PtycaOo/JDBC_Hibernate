d276ba3c40443d518bd179260ec741184f91f1e9
PtycaOo <Denkuzia5155@gmail.com>
2025-01-06 18:31:29 +0300

    #3 Made edits and added annotations

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,31 +8,37 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
 
+    @Override
     public void createUsersTable() {
         dao.createUsersTable();
         System.out.println("Таблица создана");
     }
 
+    @Override
     public void dropUsersTable() {
         dao.dropUsersTable();
         System.out.println("Таблица удалена");
 
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         dao.saveUser(name,lastName,age);
         System.out.printf("Пользователь с именем %s добавлен в базу данных\n", name);
     }
 
+    @Override
     public void removeUserById(long id) {
         dao.removeUserById(id);
         System.out.println("Запись успешно удалена");
     }
 
+    @Override
     public List<User> getAllUsers() {
         return dao.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         dao.cleanUsersTable();
         System.out.println("Таблица очищена");
